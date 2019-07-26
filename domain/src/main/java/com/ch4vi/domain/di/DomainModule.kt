2c9da16ffb3ec40c4eb13b9d9827a9aa56c79754
xavi <javier.anyo@gigigo.com>
2019-07-26 20:46:03 +0200

    New: Added domain di

---
+++ domain/src/main/java/com/ch4vi/domain/di/DomainModule.kt
@@ -0,0 +1,18 @@
+package com.ch4vi.domain.di
+
+import com.ch4vi.domain.usecase.CreateBudget
+import com.ch4vi.domain.usecase.FindLocations
+import com.ch4vi.domain.usecase.GetBudgetList
+import com.ch4vi.domain.usecase.GetCategoryList
+import com.ch4vi.domain.usecase.GetLocationList
+import com.ch4vi.domain.usecase.GetSubcategoryList
+import org.koin.dsl.module
+
+val domainModule = module {
+    factory { CreateBudget(budgetRepository = get()) }
+    factory { GetBudgetList(budgetRepository = get()) }
+    factory { FindLocations(locationRepository = get()) }
+    factory { GetLocationList(locationRepository = get()) }
+    factory { GetCategoryList(categoryRepository = get()) }
+    factory { GetSubcategoryList(categoryRepository = get()) }
+}
